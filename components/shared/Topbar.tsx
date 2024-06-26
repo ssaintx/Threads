7d8296b39627177e225b6b6d4da5e28f8354e0e2
ssaintx <saintx.git@gmail.com>
2024-06-27 03:13:47 +0500

    Update 1.0.0

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 function Topbar() {
   return (
     <nav className='topbar'>
-      <Link href='/' className='flex items-center gap-4'>
-        <Image src='/logo.svg' alt='logo' width={28} height={28} />
+      <Link href='/' className='flex items-center gap-2'>
+        <Image src='/assets/logo.png' alt='logo' width={34} height={34} />
         <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
       </Link>
 
